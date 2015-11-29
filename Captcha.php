0c6848e19b0019c7bf2e89980cf4947f4cdaeff2
ManChaisit <manlove0001@gmail.com>
2015-11-29 11:23:30 +0700

    Create TestOperand

--- Captcha.php
+++ Captcha.php
@@ -1,23 +1,93 @@
 <?php
 	class Captcha 
 	{
-		public function getQuestion($pattern_number)
+		private $pattern;
+		private $right_operand;
+		private $left_operand;
+
+
+		public function getTextNumber($number_key)
 		{
-			return "1 + Five";
+
+			$text_number = [
+			0 => 'zero',
+			1 => 'one',
+			2 => 'two',
+			3 => 'three',
+			4 => 'four',
+			5 => 'five',
+			6 => 'six',
+			7 => 'seven',
+			8 => 'eight',
+			9 => 'nine',
+			10 => 'ten',
+
+			];
+			return $text_number[$number_key];
 		}
 
-		public function getOperator()
+		public function setPattern($pattern)
 		{
+			$this->pattern = $pattern;
+		}
+
+		public function setRightOperand($number)
+		{
+			$this->right_operand = $number;
+		}
+
+		public function getRightOperand()
+		{
+			if($this->pattern == 1 ){
+				$text_number = $this->getTextNumber($this->right_operand);
+				//$this->right_operand = 'Five';
+				return $text_number;
+			}
+			return $this->right_operand;
 			
 		}
 
-		public function getNumber()
+		public function setLeftOperand($number)
 		{
-			
+			$this->left_operand = $number;
 		}
 
-		public function getTextNumber()
+		public function getLeftOperand()
 		{
+			if($this->pattern == 1 ){
+				$this->left_operand = 3;
+			}
+			return $this->left_operand;
 			
 		}
+		public function getOperator($type)
+		{
+
+			$opertor =[
+			1 => '+',
+			2 => '-',
+			];
+			return $opertor[$type];
+		}
+		
+
+		// public function getQuestion($pattern_number)
+		// {
+		// 	return "1 + Five";
+		// }
+
+		// public function getOperator()
+		// {
+			
+		// }
+
+		// public function getNumber()
+		// {
+			
+		// }
+
+		// public function getTextNumber()
+		// {
+			
+		//}
 	}	
--- CaptchaTest.php
+++ CaptchaTest.php
@@ -1,9 +1,59 @@
 <?php
 class CaptchaTest extends PHPUnit_Framework_TestCase
 {
-	public function testTextNumberEnter1ReturnOne()
+	// public function testTextNumberEnter1ReturnOne()
+	// {
+	// 	$captcha = new Captcha(1);
+	// 	$this->assertEquals('one',$Captcha->getTextNumber(1));
+	// }
+	public function testGetTextNumberWiht6ReturnSix()
 	{
-		$this->assertEquals('one',$Captcha->getTextNumber);
+
+		$captcha = new Captcha();
+		$this->assertEquals('six',$captcha->getTextNumber(6));
+	}
+
+	public function testGetTextNumberWiht10ReturnTen()
+	{
+
+		$captcha = new Captcha();
+		$this->assertEquals('ten',$captcha->getTextNumber(10));
+	}
+
+
+	public function testSetRightOperandPattern1Wiht5ReturnFive()
+	{
+		$captcha = new Captcha();
+		$captcha->setPattern(1);
+		$captcha->setRightOperand(5);
+		$this->assertEquals('five',$captcha->getRightOperand());
+	}
+
+
+	public function testSetRightOperandPattern2Wiht4Return4()
+	{
+		
+	}
+
+	public function testSetLeftOperandPattern2Wiht7ReturnSeven()
+	{
+
+	}
+
+
+
+
+	public function testGetOperator1ReturnPlusOperator()
+	{
+		$captcha = new Captcha();
+		$this->assertEquals('+',$captcha->getOperator(1));
+
+	}
+
+	public function testGetOperator2ReturnMinusOperator()
+	{
+		$captcha = new Captcha();
+		$this->assertEquals('-',$captcha->getOperator(2));
 	}
 
 }
